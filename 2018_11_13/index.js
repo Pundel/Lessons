561d3c2c06fcbddbc5a0778e85692359b8002871
Pundel <yakush93@gmail.com>
2018-11-20 23:52:42 +0200

    update. New lessons

--- 2018_11_13/index.js
+++ 2018_11_13/index.js
@@ -8,10 +8,15 @@ header.appendChild(document.createTextNode(input));
 var numP = parseInt(prompt('How many paragraph?'));
 
 for (var i = 1; i <= numP; i++) {
-	input = prompt('Enter paragraph ' + i);
+	input = prompt('Enter paragraph ' + i, i + ")");
 	var newP = document.createElement('p');
 	newP.innerText = input;
 
 	content.appendChild(newP);
 
 }
+
+newP.innerText = "first paragraph";
+var allBefore = content.getElementsByTagName('p')[0];
+
+content.insertBefore(newP, allBefore);
